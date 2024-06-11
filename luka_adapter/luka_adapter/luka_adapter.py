ae255cd51557335e1d61092fdf75989bc3a27b7d
Loy van Beek <loy.vanbeek@4am-robotics.com>
2024-06-11 10:33:31 +0200

    Actually start LukaAdapter ROS node

--- luka_adapter/luka_adapter/luka_adapter.py
+++ luka_adapter/luka_adapter/luka_adapter.py
@@ -1,9 +1,32 @@
 import rclpy
+from rclpy.node import Node
+
+
+class LukaAdapter(Node):
+    def __init__(self) -> None:
+        super().__init__('luka_adapter')
+
+        self.get_logger().info(f"LUKA adapter started")
 
 
 def main():
     rclpy.init()
     print('Starting luka_adapter')
+
+    adapter = LukaAdapter()
+
+    try:
+        rclpy.spin(adapter)
+    except KeyboardInterrupt:
+        pass
+    except BaseException:
+        raise
+    finally:
+        adapter.on_shutdown()
+        # Destroy the node explicitly
+        # (optional - Done automatically when node is garbage collected)
+        adapter.destroy_node()
+        rclpy.shutdown()
 
 
 if __name__ == '__main__':
