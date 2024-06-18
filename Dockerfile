bfa96b04a1eb3e66b53beaebd4c08e77c0648b69
Odin-byte <dominikwerman@gmail.com>
2024-06-18 15:59:39 +0200

    WIP: Port to ROS2 Foxy and calling the move_base service using the ROS bridge.

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
-FROM osrf/ros:humble-desktop-full
+FROM osrf/ros:foxy-desktop
 
 WORKDIR /dev_ws/
 
-RUN apt update && apt install ros-humble-ament* mosquitto-clients python3-paho-mqtt python3-pytest python3-pytest-mock -y
+RUN apt update && apt install ros-foxy-ament* mosquitto-clients python3-paho-mqtt python3-pytest python3-pytest-mock -y
 # && rm -rf /var/lib/apt/lists/*
 
 ADD luka_adapter /dev_ws/src/luka_adapter
@@ -10,12 +10,13 @@ ADD vda5050_msgs /dev_ws/src/vda5050_msgs
 ADD vda5050_serializer /dev_ws/src/vda5050_serializer
 ADD vda5050_connector /dev_ws/src/vda5050_connector
 ADD vda5050_bringup /dev_ws/src/vda5050_bringup
+ADD move_base_msgs /dev_ws/src/move_base_msgs
 
-# RUN /bin/bash -c "source /opt/ros/humble/setup.bash && rosdep install --from-paths src -y --ignore-src"
-RUN /bin/bash -c "source /opt/ros/humble/setup.bash && colcon build --symlink-install"
+# RUN /bin/bash -c "source /opt/ros/foxy/setup.bash && rosdep install --from-paths src -y --ignore-src"
+RUN /bin/bash -c "source /opt/ros/foxy/setup.bash && colcon build --symlink-install"
 
 
-RUN echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
+RUN echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
 RUN echo "source /dev_ws/install/local_setup.bash" >> ~/.bashrc
 
 # When inside the docker image, run ros2 run vda5050_connector mqtt_bridge.py and ros2 run vda5050_connector vda5050_controller.py
--- luka_adapter/luka_adapter/luka_adapter.py
+++ luka_adapter/luka_adapter/luka_adapter.py
@@ -1,16 +1,19 @@
 import rclpy
 from rclpy.action import ActionServer
-from rclpy.action import ActionClient
+from rclpy.action import ActionClient, GoalResponse, CancelResponse
 from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
 from rclpy.node import Node
 
+import rclpy.time
 from vda5050_connector.action import NavigateToNode
 from vda5050_connector.action import ProcessVDAAction
 from vda5050_connector.srv import GetState
 from vda5050_connector.srv import SupportedActions
 
+from action_msgs.msg import GoalStatus
 from geometry_msgs.msg import PoseStamped
 from geometry_msgs.msg import PoseWithCovarianceStamped
+from move_base_msgs.action import MoveBase
 from vda5050_msgs.msg import Node as VDANode
 from vda5050_msgs.msg import AGVPosition as VDAAGVPosition
 from vda5050_msgs.msg import Velocity as VDAVelocity
@@ -20,16 +23,14 @@ from vda5050_msgs.msg import CurrentAction as VDACurrentAction
 
 class LukaAdapter(Node):
     def __init__(self) -> None:
-        super().__init__('luka_adapter')
+        super().__init__("luka_adapter")
 
         self.get_logger().info(f"LUKA adapter starting")
 
         self._manufacturer_name = "robots"
         self._robot_name = "robot_1"
 
-        base_interface_name = (
-            f"uagv/{self._manufacturer_name}/{self._robot_name}/"
-        )
+        base_interface_name = f"uagv/{self._manufacturer_name}/{self._robot_name}/"
 
         self.get_adapter_state_srv = self.create_service(
             srv_type=GetState,
@@ -48,7 +49,9 @@ class LukaAdapter(Node):
             node=self,
             action_type=NavigateToNode,
             action_name=base_interface_name + "adapter/nav_to_node",
-            execute_callback=self.navigate_to_node_callback,
+            execute_callback=self.navigate_to_node_execute_callback,
+            goal_callback=self.navigate_to_node_goal_callback,
+            cancel_callback=self.navigate_to_node_cancel_callback,
             callback_group=nav_to_node_action_client_cb_group,
         )
 
@@ -57,6 +60,11 @@ class LukaAdapter(Node):
             action_type=ProcessVDAAction,
             action_name=base_interface_name + "adapter/vda_action",
             execute_callback=self.process_vda_action_callback,
+        )
+
+        # Move Base Action Client
+        self._move_base_client = ActionClient(
+            node=self, action_type=MoveBase, action_name="/move_base"
         )
 
         self.get_logger().info(f"LUKA adapter started")
@@ -72,17 +80,103 @@ class LukaAdapter(Node):
         # response.state = order_state
         return response
 
-    def navigate_to_node_callback(self, goal_handle):
+    def navigate_to_node_goal_callback(self, goal_request):
+        self.get_logger().info(f"Received goal request: {goal_request}")
+        return GoalResponse.ACCEPT
+
+    def navigate_to_node_cancel_callback(self, goal_handle):
+        self.get_logger().info(f"Received cancel request")
+        return CancelResponse.ACCEPT
+
+    def navigate_to_node_execute_callback(self, goal_handle):
         node = goal_handle.request.node
 
         self.get_logger().info(
             f"Navigating to node '{goal_handle.request.node}', edge '{goal_handle.request.edge}'"
         )
 
-        goal_handle.succeed()
+        self.get_logger().info(
+            f"Got position: x = {goal_handle.request.node.node_position.x}, y = {goal_handle.request.node.node_position.y}"
+        )
+
+        # Populate the Pose msg
+        target_pose = PoseStamped()
+        target_pose.header.frame_id = "map"
+
+        target_pose.pose.position.x = goal_handle.request.node.node_position.x
+        target_pose.pose.position.y = goal_handle.request.node.node_position.y
+        target_pose.pose.position.z = 0.0
+
+        target_pose.pose.orientation.w = goal_handle.request.node.node_position.theta
+
+        # Start periodic feedback
+        self._feedback_timer = self.create_timer(
+            1.0, lambda: self.send_feedback(goal_handle)
+        )
+
+        self.get_logger().info("Sending request")
+        # Initialize success flag
+        self._move_base_success = None
+
+        # Send Move Base goal asynchronously
+        self.send_move_base_goal(target_pose)
+
+        # Wait for move_base_goal_response_callback to set _move_base_success
+        while rclpy.ok() and self._move_base_success == None:
+            if self._move_base_success != None:
+                break
+
+        if self._move_base_success:
+            goal_handle.succeed()
+            self.get_logger().info("Navigation to node succeeded")
+        else:
+            goal_handle.abort()
+            self.get_logger().info("Navigation to node failed")
 
         result = NavigateToNode.Result()
         return result
+
+    def send_feedback(self, goal_handle):
+        if goal_handle.is_cancel_requested or goal_handle.status in [
+            GoalResponse.STATUS_SUCCEEDED,
+            GoalResponse.STATUS_ABORTED,
+        ]:
+            self._feedback_timer.cancel()
+            return
+
+        feedback_msg = NavigateToNode.Feedback()
+        # Get current position of the AGV
+        current_position = self.get_current_position()
+
+        feedback_msg.position.position_initialized = True
+        feedback_msg.position.localization_score = (
+            0.5  # How good is the quality of the position estimate -> 1.0 = perfect
+        )
+        feedback_msg.position.deviation_range = 0.01  # in meters
+        feedback_msg.position.x = current_position.pose.position.x
+        feedback_msg.position.y = current_position.pose.position.y
+        feedback_msg.position.theta = current_position.pose.orientation.w
+
+        # Publish feedback
+        goal_handle.publish_feedback(feedback_msg)
+
+        # Log feedback
+        self.get_logger().info(f"Sending feedback: {current_position}")
+
+    def get_current_position(self):
+        # Here you should retrieve the actual current position of your AGV.
+        # For demonstration purposes, let's return a dummy position.
+        current_position = PoseStamped()
+        current_position.header.stamp = self.get_clock().now().to_msg()
+        current_position.header.frame_id = "map"
+
+        # Replace these with actual current position values
+        current_position.pose.position.x = 1.0
+        current_position.pose.position.y = 2.0
+        current_position.pose.position.z = 0.0
+        current_position.pose.orientation.w = 1.0
+
+        return current_position
 
     def process_vda_action_callback(self, goal_handle):
         action = goal_handle.request.action
@@ -105,10 +199,54 @@ class LukaAdapter(Node):
 
         return result
 
+    # Move Base Action client functions
+    def send_move_base_goal(self, target_pose):
+        goal_msg = MoveBase.Goal()
+        goal_msg.target_pose = target_pose
+
+        self.get_logger().info("Waiting for server")
+        self._move_base_client.wait_for_server()
+
+        self.get_logger().info("Sending goal request")
+        self._send_goal_future = self._move_base_client.send_goal_async(
+            goal_msg, feedback_callback=self.move_base_feedback_callback
+        )
+
+        self._send_goal_future.add_done_callback(self.move_base_goal_response_callback)
+
+    def move_base_goal_response_callback(self, future):
+        goal_handle = future.result()
+        if not goal_handle.accepted:
+            self.get_logger().info("Goal rejected!")
+            self._move_base_success = False
+            return
+
+        self.get_logger().info("Goal accepted!")
+
+        self._get_result_future = goal_handle.get_result_async()
+        self._get_result_future.add_done_callback(self.move_base_get_result_callback)
+
+    def move_base_feedback_callback(self, feedback):
+        self.get_logger().info(
+            "Received feedback: {0}".format(feedback.feedback.sequence)
+        )
+
+    def move_base_get_result_callback(self, future):
+        result = future.result().result
+        status = future.result().status
+        if status == GoalStatus.STATUS_SUCCEEDED:
+            self.get_logger().info(
+                "Goal succeeded! Result: {0}".format(result.sequence)
+            )
+            self._move_base_success = True
+        else:
+            self.get_logger().info("Goal failed with status: {0}".format(status))
+            self._move_base_success = False
+
 
 def main():
     rclpy.init()
-    print('Starting luka_adapter')
+    print("Starting luka_adapter")
 
     adapter = LukaAdapter()
 
@@ -126,5 +264,5 @@ def main():
         rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
